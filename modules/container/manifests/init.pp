8a0309a07654a6bdf778fa903859c1ddcbaff95e
Felix Dörre <felix@dogcraft.de>
2017-03-12 06:51:29 -0400

    fix: remove getty-services
    Change-Id: I36b9c1014f83010addd9ccc13b018b5eaae924af

--- modules/container/manifests/init.pp
+++ modules/container/manifests/init.pp
@@ -13,6 +13,26 @@ class container::contained {
   file {'/certified':
     content => ''
   }
+  service {'getty@tty1':
+    ensure => 'stopped',
+    enable => 'mask'
+  }
+  service {'getty@tty2':
+    ensure => 'stopped',
+    enable => 'mask'
+  }
+  service {'getty@tty3':
+    ensure => 'stopped',
+    enable => 'mask'
+  }
+  service {'getty@tty4':
+    ensure => 'stopped',
+    enable => 'mask'
+  }
+  service {'console-getty':
+    ensure => 'stopped',
+    enable => 'mask'
+  }
 }
 class container::ssh {
   package { [ 'openssh-client', 'openssh-server']:
