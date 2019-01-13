b7f1b1ee842b3d37e74b3002b02368537199d0b9
INOPIAE <m.maengel@inopiae.de>
2019-01-13 07:50:14 +0100

    upd: reorder entries by ascending ip numbers
    Change-Id: Ic70976dc7ef6fdf4a040cacc8c6f35ec6a098369

--- environments/production/manifests/ip.pp
+++ environments/production/manifests/ip.pp
@@ -1,13 +1,13 @@
 $ips = {
    front-nginx => '10.0.3.13',
+   postgres => '10.0.3.14',
    gigi => '10.0.3.15',
    cassiopeia => '10.0.3.16',
    exim => '10.0.3.17',
    hop => '10.0.3.18',
    quiz => '10.0.3.19',
-   postgres => '10.0.3.14',
-   motion => '10.0.3.22',
-   gitweb => '10.0.3.20'}
+   gitweb => '10.0.3.20',
+   motion => '10.0.3.22'}
 
 $ipsv6 = {
    front-nginx => 'fc00:1::d',
@@ -18,8 +18,8 @@ $ipsv6 = {
    exim => 'fc00:1::11',
    hop => 'fc00:1::12',
    quiz => 'fc00:1::13',
-   motion => 'fc00:1::16',
-   gitweb => 'fc00:1::14'}
+   gitweb => 'fc00:1::14',
+   motion => 'fc00:1::16'}
 
 $passwords = {
    postgres => {
